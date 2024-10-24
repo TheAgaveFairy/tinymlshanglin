07ef6ff7b5e021f912d7fd9d82a0863024357eaa
Paul Dutton <jodgey4@gmail.com>
2024-10-23 22:04:34 -0700

    new indices uploaded as zips

--- ModelDesign/run.sh
+++ ModelDesign/run.sh
@@ -1,8 +1,10 @@
 ### train
-python3 train_1D.py -a simple5a --epoch 50 --lr 0.01 --loss f1 --batchsz 256 #--ext _Sf1bz256lr0.01
+python3 train_1D.py  --lr 0.01 --loss f1 --batchsz 256 --ext _newidx
+
+echo "TRAINING COMPLETE. TESTING NOW."
 
 ### test
-python3 test_1D.py --net_name saved_models/IEGM_net_Sf1bz256lr0.01.pkl -b 1
+python3 test_1D.py --net_name saved_models/IEGM_net_newidx.pkl -b 1
 
 ### convert 
 #python3 pt2onnx.py IEGM_net_Sf1bz256lr0.01
